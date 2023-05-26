8e047c376cf61a6166144062b75d76abb60a0ac0
Michael Yugcha <mgyugcha@gmail.com>
2023-05-26 11:55:33 -0500

    feat: prettier y eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import axios from "axios";
-import Link from "next/link";
-import $rules from "@/assets/$rules";
-import { Inter } from "next/font/google";
-import { useUtils } from "@/assets/utils";
-import { Project } from "@/types/Projects";
-import { useEffect, useState } from "react";
-import { DeleteOutlined, EyeOutlined } from "@ant-design/icons";
+import axios from 'axios'
+import Link from 'next/link'
+import $rules from '@/assets/$rules'
+import { useRouter } from 'next/router'
+import { useUtils } from '@/assets/utils'
+import { Project } from '@/types/Projects'
+import { useEffect, useState } from 'react'
+import { DeleteOutlined, EyeOutlined } from '@ant-design/icons'
 import {
   Button,
   Form,
@@ -15,36 +15,33 @@ import {
   Space,
   Table,
   Typography,
-} from "antd";
-import { useRouter } from "next/router";
-
-const inter = Inter({ subsets: ["latin"] });
+} from 'antd'
 
 export default function Home() {
-  const { showError } = useUtils();
-  const [abrirModal, setAbrirModal] = useState(false);
-  const [form] = Form.useForm();
-  const router = useRouter();
-  const [datos, setDatos] = useState<Project[]>([]);
+  const { showError } = useUtils()
+  const [abrirModal, setAbrirModal] = useState(false)
+  const [form] = Form.useForm()
+  const router = useRouter()
+  const [datos, setDatos] = useState<Project[]>([])
 
   const init = async () => {
-    const { data } = await axios.get("/api/projects");
-    setDatos(data);
-  };
+    const { data } = await axios.get('/api/projects')
+    setDatos(data)
+  }
 
   const onSubmit = async (values: Project) => {
     try {
-      const { data } = await axios.post<Project>("/api/projects", values);
-      form.resetFields();
-      router.push("/" + data.id);
+      const { data } = await axios.post<Project>('/api/projects', values)
+      form.resetFields()
+      router.push('/' + data.id)
     } catch (err) {
-      showError(err);
+      showError(err)
     }
-  };
+  }
 
   useEffect(() => {
-    init();
-  }, []);
+    init()
+  }, [])
 
   return (
     <div>
@@ -79,14 +76,14 @@ export default function Home() {
         dataSource={datos}
         rowKey="id"
         columns={[
-          { title: "ID", align: "right", dataIndex: "id" },
-          { title: "Título", dataIndex: "titulo" },
+          { title: 'ID', align: 'right', dataIndex: 'id' },
+          { title: 'Título', dataIndex: 'titulo' },
           {
-            align: "center",
+            align: 'center',
             width: 1,
             render: (_, row) => (
               <Space.Compact size="small">
-                <Link href={"/" + row.id}>
+                <Link href={'/' + row.id}>
                   <Button title="Ver proyecto" icon={<EyeOutlined />} />
                 </Link>
                 <Popconfirm
@@ -94,12 +91,12 @@ export default function Home() {
                   description="¿Seguro que desea eliminar?"
                   onConfirm={async () => {
                     try {
-                      await axios.delete("/api/projects", {
+                      await axios.delete('/api/projects', {
                         params: { id: row.id },
-                      });
-                      await init();
+                      })
+                      await init()
                     } catch (err) {
-                      showError(err);
+                      showError(err)
                     }
                   }}
                   okText="Yes"
@@ -113,5 +110,5 @@ export default function Home() {
         ]}
       />
     </div>
-  );
+  )
 }
